da86cd97e3247ef9f55823c723ace1006078ac50
Thanh Nguyen <nguyensythanh11@gmail.com>
2024-08-07 23:26:22 +0700

    fix deploy

--- src/play/scene/PreloadScene.ts
+++ src/play/scene/PreloadScene.ts
@@ -7,52 +7,49 @@ class PreloadScene extends Scene {
     }
 
     public create(): void {
-        ResourceManager.getInstance().loadImageKey('0', '../../../assets/images/0.png')
-        ResourceManager.getInstance().loadImageKey('1', '../../../assets/images/1.png')
-        ResourceManager.getInstance().loadImageKey('2', '../../../assets/images/2.png')
-        ResourceManager.getInstance().loadImageKey('3', '../../../assets/images/3.png')
-        ResourceManager.getInstance().loadImageKey('4', '../../../assets/images/4.png')
-        ResourceManager.getInstance().loadImageKey('5', '../../../assets/images/5.png')
-        ResourceManager.getInstance().loadImageKey('6', '../../../assets/images/6.png')
-        ResourceManager.getInstance().loadImageKey('7', '../../../assets/images/7.png')
-        ResourceManager.getInstance().loadImageKey('8', '../../../assets/images/8.png')
-        ResourceManager.getInstance().loadImageKey('9', '../../../assets/images/9.png')
+        ResourceManager.getInstance().loadImageKey('0', '../../../assets/image/0.png')
+        ResourceManager.getInstance().loadImageKey('1', '../../../assets/image/1.png')
+        ResourceManager.getInstance().loadImageKey('2', '../../../assets/image/2.png')
+        ResourceManager.getInstance().loadImageKey('3', '../../../assets/image/3.png')
+        ResourceManager.getInstance().loadImageKey('4', '../../../assets/image/4.png')
+        ResourceManager.getInstance().loadImageKey('5', '../../../assets/image/5.png')
+        ResourceManager.getInstance().loadImageKey('6', '../../../assets/image/6.png')
+        ResourceManager.getInstance().loadImageKey('7', '../../../assets/image/7.png')
+        ResourceManager.getInstance().loadImageKey('8', '../../../assets/image/8.png')
+        ResourceManager.getInstance().loadImageKey('9', '../../../assets/image/9.png')
 
-        ResourceManager.getInstance().loadImageKey('ground', '../../../assets/images/base.png')
+        ResourceManager.getInstance().loadImageKey('ground', '../../../assets/image/base.png')
         ResourceManager.getInstance().loadImageKey(
             'yellowbird-downflap',
-            '../../../assets/images/yellowbird-downflap.png'
+            '../../../assets/image/yellowbird-downflap.png'
         )
         ResourceManager.getInstance().loadImageKey(
             'yellowbird-midflap',
-            '../../../assets/images/yellowbird-midflap.png'
+            '../../../assets/image/yellowbird-midflap.png'
         )
         ResourceManager.getInstance().loadImageKey(
             'yellowbird-upflap',
-            '../../../assets/images/yellowbird-upflap.png'
+            '../../../assets/image/yellowbird-upflap.png'
         )
         ResourceManager.getInstance().loadImageKey(
             'pipe-green-down',
-            '../../../assets/images/pipe-green-down.png'
+            '../../../assets/image/pipe-green-down.png'
         )
         ResourceManager.getInstance().loadImageKey(
             'pipe-green',
-            '../../../assets/images/pipe-green.png'
+            '../../../assets/image/pipe-green.png'
         )
-        ResourceManager.getInstance().loadImageKey('message', '../../../assets/images/message.png')
+        ResourceManager.getInstance().loadImageKey('message', '../../../assets/image/message.png')
         ResourceManager.getInstance().loadImageKey(
             'background-day',
-            '../../../assets/images/background-day.png'
+            '../../../assets/image/background-day.png'
         )
         ResourceManager.getInstance().loadImageKey(
             'background-night',
-            '../../../assets/images/background-night.png'
+            '../../../assets/image/background-night.png'
         )
-        ResourceManager.getInstance().loadImageKey(
-            'gameover',
-            '../../../assets/images/gameover.png'
-        )
-        ResourceManager.getInstance().loadImageKey('sprite', '../../../assets/images/sprite.png')
+        ResourceManager.getInstance().loadImageKey('gameover', '../../../assets/image/gameover.png')
+        ResourceManager.getInstance().loadImageKey('sprite', '../../../assets/image/sprite.png')
     }
 
     public update(deltaTime: number): void {
